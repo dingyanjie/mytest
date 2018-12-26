6b726beb2eaccb33c2f90def87a6de723f581e32
ding <183339165@qq.com>
2018-12-26 11:43:19 +0800

    添加测试项目

---
+++ my-test/server/src/main/java/com/my/SpringBootServer.java
@@ -0,0 +1,14 @@
+package com.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ding on 2018/12/25.
+ */
+@SpringBootApplication
+public class SpringBootServer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServer.class,args);
+    }
+}
